93f23cb586b400fcd57584a7fda90ebe77a76d35
Kevin Briggs <kevinbriggs1@gmail.com>
2019-09-01 21:47:57 -0400

    multiple leds

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -4,22 +4,48 @@ require 'rpi_gpio'
 class SensorsController < ApplicationController
   include LedControl
   skip_before_action :verify_authenticity_token
+  runLoop = false
 
   def temp
-    RPi::GPIO.set_numbering :bcm   
-    RPi::GPIO.setup 17, :as => :output
-    RPi::GPIO.set_low 17
+    setupLEDs
   end
 
   def turn_on
-    RPi::GPIO.set_numbering :bcm   
-    RPi::GPIO.setup 17, :as => :output
-    RPi::GPIO.set_high 17
+    RPi::GPIO.set_high params['led']
   end
 
-  def turn_off
-    RPi::GPIO.set_numbering :bcm   
+  def turn_off    
+    RPi::GPIO.set_low params['led']
+  end
+
+  def blink
+  end
+
+  def loop_leds
+    runLoop = params['runLoop']
+      
+      # while runLoop
+      #   RPi::GPIO.set_high 17
+      #   sleep 1
+      #   RPi::GPIO.set_low 17
+      #   sleep 1
+      # end
+        
+      if runLoop = false 
+        RPi::GPIO.set_low 17
+      end
+  end
+
+  def stop_loop
+    runLoop = false
+  end
+
+
+
+  def setupLEDs
+    RPi::GPIO.set_numbering :bcm  
     RPi::GPIO.setup 17, :as => :output
-    RPi::GPIO.set_low 17
+    RPi::GPIO.setup 25, :as => :output
+    RPi::GPIO.setup 16, :as => :output
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'sensors/temp'
   match '/sensors/turn_on' => 'sensors#turn_on', via: :post
   match '/sensors/turn_off' => 'sensors#turn_off', via: :post
+  match '/sensors/loopleds' => 'sensors#loop_leds', via: :post
+  match '/sensors/stoploop' => 'sensors#stop_loop', via: :post
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
