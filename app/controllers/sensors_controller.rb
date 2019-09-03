16acf9ef9fee445c3e3fbeadbc2471ea9e39325b
Kevin Briggs <kevinbriggs1@gmail.com>
2019-09-02 23:45:10 -0400

    loop working

--- app/controllers/sensors_controller.rb
+++ app/controllers/sensors_controller.rb
@@ -2,6 +2,8 @@ require "#{Rails.root}/app/lib/led_control"
 require 'rpi_gpio'
 
 class SensorsController < ApplicationController
+  @@thread = Thread.new {}
+  @@pause_value = 0.5
   include LedControl
   skip_before_action :verify_authenticity_token
   runLoop = false
@@ -14,30 +16,29 @@ class SensorsController < ApplicationController
     RPi::GPIO.set_high params['led']
   end
 
-  def turn_off    
+  def turn_off
+    puts 'turning offzzz'    
     RPi::GPIO.set_low params['led']
   end
 
-  def blink
-  end
-
   def loop_leds
+    @@thread.exit
+    turn_LEDs_off
     runLoop = params['runLoop']
-      
-      # while runLoop
-      #   RPi::GPIO.set_high 17
-      #   sleep 1
-      #   RPi::GPIO.set_low 17
-      #   sleep 1
-      # end
-        
-      if runLoop = false 
-        RPi::GPIO.set_low 17
+    
+    if runLoop
+      @@thread = Thread.new do
+          loop do
+            blink_led(17)
+            blink_led(25)
+            blink_led(16)
+            end
       end
+    end
   end
 
-  def stop_loop
-    runLoop = false
+  def change_pause_value
+    @@pause_value = params['pauseValue'].to_f
   end
 
 
@@ -47,5 +48,20 @@ class SensorsController < ApplicationController
     RPi::GPIO.setup 17, :as => :output
     RPi::GPIO.setup 25, :as => :output
     RPi::GPIO.setup 16, :as => :output
+    turn_LEDs_off
   end
+
+  def turn_LEDs_off
+    RPi::GPIO.set_low 17
+    RPi::GPIO.set_low 25
+    RPi::GPIO.set_low 16
+  end
+
+  def blink_led(led) 
+    RPi::GPIO.set_high led
+    sleep @@pause_value
+    RPi::GPIO.set_low led
+  end
+
+
 end
