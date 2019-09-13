eb09213e012c365d9cd7fd0ce0a236c0adf4c4fb
kevin578 <kevinbriggs1@gmail.com>
2019-09-13 01:26:43 +0100

    clicking is buggy

--- app/assets/javascripts/sidebar.js
+++ app/assets/javascripts/sidebar.js
@@ -1,5 +1,29 @@
+let currentURL = window.location.pathname.substring(1);
+
+//$(`#${currentURL}-link`).addClass('')
+
 $(window).ready(()=> {
-    $(document).on('click','.sidebar__link',(e)=> {
-        console.log(window.location.pathname)
-    })
+    setIndicator($('.sidebar__link') )
+    $(document).on('click', '.sidebar__link', (e)=> handleClick(e));
 })
+
+function setIndicator(arr) {
+    for (let i = 0; i < arr.length; i++) {
+        const pageID = arr[i].id;
+        const pageURL = pageID.split('-')[0];
+        if (pageURL === currentURL) {
+            $(`#${pageID}`).children().addClass('sidebar__img--active')
+            $(`#${pageID}`).children().removeClass('sidebar__img--inactive')
+        }
+        else {
+            $(`#${pageID}`).children().addClass('sidebar__img--inactive')
+            $(`#${pageID}`).children().removeClass('sidebar__img--active') 
+        }
+    }
+}
+
+function handleClick(e) {
+    const clickedId = e.target.id || $(e.target).parents('.sidebar__link').attr('id');
+    currentURL = clickedId.split('-')[0];
+    setIndicator($('.sidebar__link') )
+}
